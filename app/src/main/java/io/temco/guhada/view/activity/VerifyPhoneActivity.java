8ead6ee28ccf746e1d6e70d58d816cc3f81db0dc
HyeyeonPark <hyeyeon.park@temco.io>
2019-05-24 14:46:16 +0900

    휴대폰 본인인증 결과 값 추출

--- app/src/main/java/io/temco/guhada/view/activity/VerifyPhoneActivity.java
+++ app/src/main/java/io/temco/guhada/view/activity/VerifyPhoneActivity.java
@@ -1,6 +1,9 @@
 package io.temco.guhada.view.activity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.util.Log;
+import android.view.View;
 import android.webkit.ClientCertRequest;
 import android.webkit.CookieManager;
 import android.webkit.WebResourceRequest;
@@ -8,6 +11,12 @@ import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import android.widget.Toast;
+
+import org.apache.commons.text.StringEscapeUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
 import io.temco.guhada.R;
 import io.temco.guhada.common.Type;
@@ -64,8 +73,7 @@ public class VerifyPhoneActivity extends BindActivity<ActivityVerifyphoneBinding
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
                 if (request.getUrl().toString().equals("https://nice.checkplus.co.kr/CheckPlusSafeModel/checkplus.cb?m=checkplusSerivce_resultSend")) {
-                    setResult(RESULT_OK);
-                    finish();
+                    mBinding.webviewVerifyphone.setVisibility(View.GONE);
                 }
                 return super.shouldOverrideUrlLoading(view, request);
             }
@@ -73,6 +81,31 @@ public class VerifyPhoneActivity extends BindActivity<ActivityVerifyphoneBinding
             @Override
             public void onPageFinished(WebView view, String url) {
                 super.onPageFinished(view, url);
+
+                if (url.equals("http://13.209.10.68/phoneCertification/success")) {
+                    mBinding.webviewVerifyphone.evaluateJavascript("(function(){return window.document.body.outerHTML})();",
+                            html -> {
+                                String s = StringEscapeUtils.unescapeJava(html);
+                                Document document = Jsoup.parse(s);
+                                Element table = document.select("table").first();
+                                Elements trs = table.select("tr");
+                                String name = trs.get(4).select("td").get(1).text();
+                                String birth = trs.get(7).select("td").get(1).text();
+                                String phoneNumber = trs.get(10).select("td").get(1).text();
+
+                                /**
+                                 *  <휴대폰번호> 는 업체에 따로 신청해야해서 추후에 필드값 넘어올 예정 (05.24)
+                                 */
+
+                                Log.e("본인인증 완료", "name: " + name + "; birth: " + birth + "; phoneNumber: " + phoneNumber);
+                                Intent intent = getIntent();
+                                intent.putExtra("name", name);
+                                intent.putExtra("phoneNumber", phoneNumber);
+                                setResult(RESULT_OK, intent);
+                                finish();
+                            }
+                    );
+                }
             }
         };
 
@@ -80,6 +113,7 @@ public class VerifyPhoneActivity extends BindActivity<ActivityVerifyphoneBinding
         mBinding.webviewVerifyphone.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
         mBinding.webviewVerifyphone.getSettings().setSupportMultipleWindows(true);
         mBinding.webviewVerifyphone.setWebViewClient(client);
+
         mBinding.webviewVerifyphone.loadUrl(URL);
     }
 
