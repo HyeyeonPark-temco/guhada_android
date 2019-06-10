995ce89faa9d2f09b26d81cf203c1b9f18bb12a1
kong <bumshik.kong@temco.io>
2019-06-10 11:21:53 +0900

    - 브랜드 목록 화면 디자인 가이드 수정

--- app/src/main/java/io/temco/guhada/view/adapter/BrandListAdapter.java
+++ app/src/main/java/io/temco/guhada/view/adapter/BrandListAdapter.java
@@ -44,9 +44,9 @@ public class BrandListAdapter extends StickyHeaderRecyclerAdapter<BaseBrandViewH
     private String mCurrentSection;
     private boolean mIsShowSection = false;
     private int mHeaderHeight;
+    private float mTopPadding;
     private float mPointTextSize;
     private float mLetterTextSize;
-    private float mLetterTextPadding;
     private float mSectionThumbTextSize;
     private float mSectionThumbSize;
     private float mSectionThumbPadding;
@@ -68,9 +68,9 @@ public class BrandListAdapter extends StickyHeaderRecyclerAdapter<BaseBrandViewH
         mContext = context;
         // Size
         mHeaderHeight = context.getResources().getDimensionPixelSize(R.dimen.view_fast_scroll_brand_header_row);
+        mTopPadding = context.getResources().getDimension(R.dimen.padding_fast_scroll_brand_top);
         mPointTextSize = context.getResources().getDimension(R.dimen.text_8);
         mLetterTextSize = context.getResources().getDimension(R.dimen.text_10);
-        mLetterTextPadding = context.getResources().getDimension(R.dimen.padding_fast_scroll_brand_text);
         mSectionThumbTextSize = context.getResources().getDimension(R.dimen.text_30);
         mSectionThumbSize = context.getResources().getDimension(R.dimen.view_fast_scroll_brand_thumb);
         mSectionThumbPadding = context.getResources().getDimension(R.dimen.padding_fast_scroll_brand_section);
@@ -169,7 +169,7 @@ public class BrandListAdapter extends StickyHeaderRecyclerAdapter<BaseBrandViewH
 
     @Override
     public float getTopPadding() {
-        return mLetterTextPadding;
+        return mTopPadding;
     }
 
     @Override
