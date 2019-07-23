236c6ffd4e5fe22cb019a8247f9ff6d9acf2abff
Pj1074 <jungho.park@temco.io>
2019-07-23 14:06:32 +0900

    package 위치 이동

---
+++ app/src/main/java/io/temco/guhada/data/viewmodel/main/MainListPageViewModel.kt
@@ -0,0 +1,16 @@
+package io.temco.guhada.data.viewmodel.main
+
+import android.content.Context
+import io.temco.guhada.data.viewmodel.base.BaseObservableViewModel
+
+/**
+ * @author park jungho
+ * 19.07.18
+ *
+ * 메인 Fragment ViewModel
+ */
+class MainListPageViewModel(val context : Context) : BaseObservableViewModel() {
+
+
+
+}
