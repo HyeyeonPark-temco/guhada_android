bd956e9337dd8783308e4e67e497642c3c1771f4
HyeyeonPark <hyeyeon.park@temco.io>
2019-09-02 14:22:51 +0900

    text list bottom sheet fragment 구현 완료

--- app/src/main/java/io/temco/guhada/view/fragment/ListBottomSheetFragment.kt
+++ app/src/main/java/io/temco/guhada/view/fragment/ListBottomSheetFragment.kt
@@ -9,13 +9,20 @@ import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.bottomsheet.BottomSheetDialogFragment
 import io.temco.guhada.R
-import io.temco.guhada.databinding.FragmentSellerstoreMoreBinding
+import io.temco.guhada.databinding.FragmentListbottomsheetBinding
 import io.temco.guhada.databinding.ItemListbottomsheetBinding
 import io.temco.guhada.view.holder.base.BaseViewHolder
 
+/**
+ * Text List BottomSheetFragment
+ * @author Hyeyeon Park
+ * @since 2019.08.30
+ */
 class ListBottomSheetFragment(val mContext: Context) : BottomSheetDialogFragment() {
-    private lateinit var mBinding: FragmentSellerstoreMoreBinding
+    private lateinit var mBinding: FragmentListbottomsheetBinding
+    var mTitle: String = ""
     var mList = mutableListOf<String>()
+    lateinit var mListener: ListBottomSheetListener
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,14 +30,20 @@ class ListBottomSheetFragment(val mContext: Context) : BottomSheetDialogFragment
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_sellerstore_more, container, false)
-        mBinding.recyclerView.adapter = ListBottomSheetAdapter().apply { this.mList = this@ListBottomSheetFragment.mList }
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_listbottomsheet, container, false)
+        mBinding.title = mTitle
+        mBinding.imagebuttonListbottomsheetClose.setOnClickListener { dismiss() }
+        mBinding.recyclerView.adapter = ListBottomSheetAdapter().apply {
+            this.mList = this@ListBottomSheetFragment.mList
+            this.mListener = this@ListBottomSheetFragment.mListener
+        }
         mBinding.executePendingBindings()
         return mBinding.root
     }
 
     class ListBottomSheetAdapter : RecyclerView.Adapter<ListBottomSheetAdapter.Holder>() {
         var mList = mutableListOf<String>()
+        lateinit var mListener: ListBottomSheetListener
 
         override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder =
                 Holder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_listbottomsheet, parent, false))
@@ -42,9 +55,18 @@ class ListBottomSheetFragment(val mContext: Context) : BottomSheetDialogFragment
 
         inner class Holder(mBinding: ItemListbottomsheetBinding) : BaseViewHolder<ItemListbottomsheetBinding>(mBinding.root) {
             fun bind(text: String) {
+                mBinding.linearlayoutListbottomsheetContainer.setOnClickListener {
+                    mListener.onItemClick(adapterPosition)
+                    mListener.onClickClose()
+                }
                 mBinding.text = text
                 mBinding.executePendingBindings()
             }
         }
+    }
+
+    interface ListBottomSheetListener {
+        fun onItemClick(position: Int)
+        fun onClickClose()
     }
 }
