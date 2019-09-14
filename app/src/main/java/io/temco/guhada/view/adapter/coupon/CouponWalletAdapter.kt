0008fa26ee8931849a28ef668a456c273d8d186b
HyeyeonPark <hyeyeon.park@temco.io>
2019-09-14 13:15:30 +0900

    쿠폰 선택-총 상품 수, 총 주문금액 추가, 쿠폰 선택 adapter 작업 중

--- app/src/main/java/io/temco/guhada/view/adapter/coupon/CouponWalletAdapter.kt
+++ app/src/main/java/io/temco/guhada/view/adapter/coupon/CouponWalletAdapter.kt
@@ -16,8 +16,11 @@ import io.temco.guhada.view.holder.base.BaseViewHolder
  * @since 2019.09.13
  */
 class CouponWalletAdapter : RecyclerView.Adapter<CouponWalletAdapter.Holder>() {
-    lateinit var mViewModel : CouponSelectDialogViewModel
+    lateinit var mViewModel: CouponSelectDialogViewModel
     var mList = mutableListOf<CouponWallet>()
+    var onBind = false
+    var prevPos = -1
+    var selectedPos = -1
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): Holder =
             Holder(DataBindingUtil.inflate(LayoutInflater.from(parent.context), R.layout.item_couponselect_coupon, parent, false))
@@ -30,6 +33,20 @@ class CouponWalletAdapter : RecyclerView.Adapter<CouponWalletAdapter.Holder>() {
 
     inner class Holder(binding: ItemCouponselectCouponBinding) : BaseViewHolder<ItemCouponselectCouponBinding>(binding.root) {
         fun bind(item: CouponWallet) {
+            mBinding.checkboxCouponselectCoupon.setOnCheckedChangeListener { buttonView, isChecked ->
+                if (isChecked) {
+                    prevPos = selectedPos
+                    selectedPos = adapterPosition
+                }
+
+                if (!onBind) {
+                    notifyDataSetChanged()
+                }
+            }
+
+            onBind = true
+            mBinding.checkboxCouponselectCoupon.isChecked = (adapterPosition == selectedPos)
+            onBind = false
             mBinding.couponWallet = item
         }
     }
