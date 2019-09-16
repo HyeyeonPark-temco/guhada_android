97a91e2cfd8e9fa6088b0d68608a18a296f709f1
HyeyeonPark <hyeyeon.park@temco.io>
2019-09-16 12:09:54 +0900

    주문결제 쿠폰선택 ui 로직 수정

--- app/src/main/java/io/temco/guhada/view/adapter/coupon/CouponWalletAdapter.kt
+++ app/src/main/java/io/temco/guhada/view/adapter/coupon/CouponWalletAdapter.kt
@@ -2,6 +2,7 @@ package io.temco.guhada.view.adapter.coupon
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import io.temco.guhada.R
@@ -18,7 +19,6 @@ import io.temco.guhada.view.holder.base.BaseViewHolder
 class CouponWalletAdapter : RecyclerView.Adapter<CouponWalletAdapter.Holder>() {
     lateinit var mViewModel: CouponSelectDialogViewModel
     var mList = mutableListOf<CouponWallet>()
-    var onBind = false
     var prevPos = -1
     var selectedPos = -1
 
@@ -33,21 +33,16 @@ class CouponWalletAdapter : RecyclerView.Adapter<CouponWalletAdapter.Holder>() {
 
     inner class Holder(binding: ItemCouponselectCouponBinding) : BaseViewHolder<ItemCouponselectCouponBinding>(binding.root) {
         fun bind(item: CouponWallet) {
-            mBinding.checkboxCouponselectCoupon.setOnCheckedChangeListener { buttonView, isChecked ->
-                if (isChecked) {
-                    prevPos = selectedPos
-                    selectedPos = adapterPosition
-                }
-
-                if (!onBind) {
-                    notifyDataSetChanged()
-                }
+            mBinding.imageviewCouponselectCoupon.setOnClickListener {
+                prevPos = selectedPos
+                selectedPos = adapterPosition
+                (it as ImageView).setImageResource(R.drawable.radio_checked)
+                if (prevPos > -1) notifyItemChanged(prevPos)
             }
 
-            onBind = true
-            mBinding.checkboxCouponselectCoupon.isChecked = (adapterPosition == selectedPos)
-            onBind = false
+            mBinding.imageviewCouponselectCoupon.setImageResource(R.drawable.radio_select)
             mBinding.couponWallet = item
+            mBinding.executePendingBindings()
         }
     }
 }
