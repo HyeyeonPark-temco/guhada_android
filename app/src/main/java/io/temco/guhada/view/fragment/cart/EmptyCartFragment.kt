ea13251bee8c6e9f8584c7541ff856c6a2b8a48f
Pj1074 <jungho.park@temco.io>
2019-09-21 14:50:29 +0900

    빈 장바구니 레이아웃 수정

--- app/src/main/java/io/temco/guhada/view/fragment/cart/EmptyCartFragment.kt
+++ app/src/main/java/io/temco/guhada/view/fragment/cart/EmptyCartFragment.kt
@@ -1,23 +1,30 @@
 package io.temco.guhada.view.fragment.cart
 
 import android.app.Activity
+import androidx.lifecycle.Observer
 import io.temco.guhada.R
 import io.temco.guhada.common.ActivityMoveToMain
 import io.temco.guhada.common.BaseApplication
 import io.temco.guhada.common.Flag
 import io.temco.guhada.common.enum.ResultCode
+import io.temco.guhada.common.util.CommonUtil
 import io.temco.guhada.data.viewmodel.cart.EmptyCartViewModel
 import io.temco.guhada.databinding.FragmentEmptycartBinding
+import io.temco.guhada.view.adapter.cart.AddCartResultProductAdapter
 import io.temco.guhada.view.fragment.base.BaseFragment
 import io.temco.guhada.view.fragment.mypage.MyPageTabType
 
 class EmptyCartFragment : BaseFragment<FragmentEmptycartBinding>() {
+    private lateinit var mViewModel: EmptyCartViewModel
+
     override fun getBaseTag(): String = EmptyCartFragment::class.java.simpleName
 
     override fun getLayoutId(): Int = R.layout.fragment_emptycart
 
     override fun init() {
-        mBinding.viewModel = EmptyCartViewModel()
+        initViewModel()
+        mViewModel.getDeals()
+        mBinding.viewModel = mViewModel
         mBinding.executePendingBindings()
 
         mBinding.setOnClickContinue {
@@ -33,4 +40,20 @@ class EmptyCartFragment : BaseFragment<FragmentEmptycartBinding>() {
         }
 
     }
+
+
+    private fun initViewModel() {
+        mViewModel = EmptyCartViewModel()
+        mViewModel.mDealList.observe(this, Observer {
+            mBinding.recyclerviewCartRecommend.adapter = AddCartResultProductAdapter().apply {
+                this.mList = it
+                this.mClickItemTask = {dealId ->
+                    CommonUtil.startProductActivity(this@EmptyCartFragment.context as Activity, dealId)
+                    (this@EmptyCartFragment.context as Activity).finish()
+                }
+            }
+        })
+
+    }
+
 }
