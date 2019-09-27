aef3cecd8459d7dc26a3297c493128f9291e0ec5
HyeyeonPark <hyeyeon.park@temco.io>
2019-09-27 20:55:36 +0900

    [TECH-3478] 주문 취소신청, 반품신청, 교환신청 시 에러 메세지 수정

--- app/src/main/java/io/temco/guhada/data/viewmodel/mypage/delivery/CancelOrderViewModel.kt
+++ app/src/main/java/io/temco/guhada/data/viewmodel/mypage/delivery/CancelOrderViewModel.kt
@@ -4,10 +4,12 @@ import androidx.lifecycle.MutableLiveData
 import io.temco.guhada.R
 import io.temco.guhada.common.BaseApplication
 import io.temco.guhada.common.listener.OnServerListener
+import io.temco.guhada.common.util.CommonUtil
 import io.temco.guhada.common.util.ServerCallbackUtil
 import io.temco.guhada.common.util.ToastUtil
 import io.temco.guhada.data.model.CancelRequest
 import io.temco.guhada.data.model.ExpectedRefundPrice
+import io.temco.guhada.data.model.base.BaseModel
 import io.temco.guhada.data.model.order.PurchaseOrder
 import io.temco.guhada.data.server.ClaimServer
 import io.temco.guhada.data.viewmodel.base.BaseObservableViewModel
@@ -28,6 +30,14 @@ class CancelOrderViewModel : BaseObservableViewModel() {
                         successTask = {
                             if (it.data != null)
                                 this@CancelOrderViewModel.purchaseOrder.postValue(it.data as PurchaseOrder)
+                        },
+                        dataIsNull = {
+                            if (it is BaseModel<*>) {
+                                CommonUtil.debug(it.message)
+                                ToastUtil.showMessage(it.message)
+                            } else {
+                                ToastUtil.showMessage(BaseApplication.getInstance().getString(R.string.common_message_servererror))
+                            }
                         })
             }, accessToken = token, orderProdGroupId = orderProdGroupId)
         })
@@ -62,6 +72,14 @@ class CancelOrderViewModel : BaseObservableViewModel() {
                         },
                         failedTask = {
                             ToastUtil.showMessage("[${it.resultCode}] ${BaseApplication.getInstance().getString(R.string.common_message_servererror)}")
+                        },
+                        dataIsNull = {
+                            if (it is BaseModel<*>) {
+                                CommonUtil.debug(it.message)
+                                ToastUtil.showMessage(it.message)
+                            } else {
+                                ToastUtil.showMessage(BaseApplication.getInstance().getString(R.string.common_message_servererror))
+                            }
                         })
             }, accessToken = token, cancelRequest = cancelRequest)
         })
