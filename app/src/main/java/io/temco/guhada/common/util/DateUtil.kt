4310139406eaa50f4c02876a71b11afc61a6c9b2
HyeyeonPark <hyeyeon.park@temco.io>
2019-10-11 14:39:42 +0900

    [TECH-1822] 상품상세-상품문의 시간 format 변경

--- app/src/main/java/io/temco/guhada/common/util/DateUtil.kt
+++ app/src/main/java/io/temco/guhada/common/util/DateUtil.kt
@@ -110,7 +110,7 @@ object DateUtil {
     fun convertTimestamp(timestamp: Long, separator: String): String = DateTime(timestamp).toString("yyyy${separator}MM${separator}dd")
 
     @JvmStatic
-    fun convertDateTimestamp(timestamp: Long, separator: String): String = DateTime(timestamp).toString("yyyy${separator}MM${separator}dd HH:mm")
+    fun convertDateTimestamp(timestamp: Long, separator: String): String = DateTime(timestamp).toString("yyyy${separator}MM${separator}dd hh:mm")
 
     @JvmStatic
     fun getNowDateDiffMinute(date: Long): Int {
