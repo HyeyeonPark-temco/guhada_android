e328bbab557efe834fbfa416970a177aebccc5ed
HyeyeonPark <hyeyeon.park@temco.io>
2019-10-22 15:30:07 +0900

    Tracking Util, Login tracker key enum class 추가

---
+++ app/src/main/java/io/temco/guhada/common/enum/TrackingEvent.kt
@@ -0,0 +1,40 @@
+package io.temco.guhada.common.enum
+
+object TrackingEvent {
+
+    enum class Login(val eventName: String) {
+        Login_MainP_CancelButton("Login_MainP_CancelButton"),
+        Login_MainP_FindIdButton("Login_MainP_FindIdButton"),
+        Login_MainP_FindPwButton("Login_MainP_FindPwButton"),
+        Login_MainP_InputId("Login_MainP_InputId"),
+        Login_MainP_Inputpw("Login_MainP_Inputpw"),
+        Login_MainP_AutomaticLoginClick("Login_MainP_AutomaticLoginClick"),
+        Login_MainP_SaveIdClick("Login_MainP_SaveIdClick"),
+        Login_MainP_CheckOrderButton("Login_MainP_CheckOrderButton"),
+        Login_MainP_LoginButton("Login_MainP_LoginButton"),
+        Login_MainP_SignUpButton("Login_MainP_SignUpButton"),
+        Login_MainP_NaverLoginButton("Login_MainP_NaverLoginButton"),
+        Login_MainP_KakaoLoginButton("Login_MainP_KakaoLoginButton"),
+        Login_MainP_FacebookLoginButton("Login_MainP_FacebookLoginButton"),
+        Login_MainP_GoogleLoginButton("Login_MainP_GoogleLoginButton"),
+        SignUp_Page1_InputId("SignUp_Page1_InputId"),
+        SignUp_Page1_Inputpw("SignUp_Page1_Inputpw")
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
