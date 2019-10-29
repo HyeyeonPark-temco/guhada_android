d57482e42b087d35953fce8312b6d07570f3463f
Pj1074 <jungho.park@temco.io>
2019-10-29 11:44:09 +0900

    PREMIUM ITEM api product -> search  서버로 변경

--- app/src/main/java/io/temco/guhada/data/viewmodel/main/WomenListViewModel.kt
+++ app/src/main/java/io/temco/guhada/data/viewmodel/main/WomenListViewModel.kt
@@ -82,7 +82,7 @@ class WomenListRepository(val context : Context){
      * PREMIUM ITEM
      */
     private fun getPlusItem() {//getProductByPlusItem
-        ProductServer.getProductByPlusItem(unitPerPage,OnServerListener { success, o ->
+        SearchServer.getProductByPlusItem(unitPerPage,OnServerListener { success, o ->
             ServerCallbackUtil.executeByResultCode(success, o,
                     successTask = {
                         var newArrival =  (o as BaseModel<*>).data as HomeDeal
