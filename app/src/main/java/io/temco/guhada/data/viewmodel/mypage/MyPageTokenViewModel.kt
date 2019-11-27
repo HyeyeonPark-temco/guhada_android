a99b37a5bd2a0778d14901f1f81763a79c6070a6
HyeyeonPark <hyeyeon.park@temco.io>
2019-11-27 18:10:00 +0900

    마이페이지-토큰 리스트 view 구성

---
+++ app/src/main/java/io/temco/guhada/data/viewmodel/mypage/MyPageTokenViewModel.kt
@@ -0,0 +1,11 @@
+package io.temco.guhada.data.viewmodel.mypage
+
+import io.temco.guhada.data.viewmodel.base.BaseObservableViewModel
+
+/**
+ * 마이페이지 토큰 ViewModel
+ * @author Hyeyeon Park
+ * @since 2019.11.27
+ */
+class MyPageTokenViewModel : BaseObservableViewModel() {
+}
