cce5f99f95d9addb67d073bf0fd4e4cd1e3971c1
HyeyeonPark <hyeyeon.park@temco.io>
2019-12-03 13:01:27 +0900

    [TECH-5036] 마이페이지-쿠폰 더보기 버튼 > 무한스크롤 적용

--- app/src/main/java/io/temco/guhada/data/viewmodel/mypage/MyPageCouponViewModel.kt
+++ app/src/main/java/io/temco/guhada/data/viewmodel/mypage/MyPageCouponViewModel.kt
@@ -5,6 +5,7 @@ import androidx.databinding.Bindable
 import androidx.lifecycle.MutableLiveData
 import io.temco.guhada.common.listener.OnServerListener
 import io.temco.guhada.common.util.ServerCallbackUtil
+import io.temco.guhada.data.model.coupon.Coupon
 import io.temco.guhada.data.model.coupon.CouponResponse
 import io.temco.guhada.data.server.BenefitServer
 import io.temco.guhada.data.viewmodel.base.BaseObservableViewModel
@@ -38,12 +39,12 @@ class MyPageCouponViewModel(val context: Context) : BaseObservableViewModel() {
                             if (isAvailable) this.enabledCouponResponse.postValue(response)
                             else this.disabledCouponResponse.postValue(response)
                         })
-            }, accessToken = token, isAvailable = isAvailable, page = page, unitPerPage = UNIT_PER_PAGE)
+            }, accessToken = token, isAvailable = isAvailable, page = page++, unitPerPage = UNIT_PER_PAGE)
         })
     }
 
     fun onClickMore(isAvailable: Boolean) {
-        page++
+//        page++
         getCoupons(isAvailable)
     }
 }
