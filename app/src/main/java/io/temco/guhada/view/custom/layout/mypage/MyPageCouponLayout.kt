929dd6da018fcb075bd0c9017972ee0d9e5487a4
HyeyeonPark <hyeyeon.park@temco.io>
2019-12-24 17:21:52 +0900

    마이페이지-쿠폰 swipe refresh 버그 픽스

--- app/src/main/java/io/temco/guhada/view/custom/layout/mypage/MyPageCouponLayout.kt
+++ app/src/main/java/io/temco/guhada/view/custom/layout/mypage/MyPageCouponLayout.kt
@@ -3,9 +3,7 @@ package io.temco.guhada.view.custom.layout.mypage
 import android.content.Context
 import android.util.AttributeSet
 import androidx.fragment.app.FragmentActivity
-import androidx.lifecycle.Observer
 import io.temco.guhada.R
-import io.temco.guhada.data.model.coupon.Coupon
 import io.temco.guhada.data.viewmodel.mypage.MyPageCouponViewModel
 import io.temco.guhada.databinding.CustomlayoutMypageCouponBinding
 import io.temco.guhada.view.adapter.base.BaseFragmentPagerAdapter
@@ -27,64 +25,33 @@ class MyPageCouponLayout constructor(
         defStyleAttr: Int = 0
 ) : BaseListLayout<CustomlayoutMypageCouponBinding, MyPageCouponViewModel>(context, attrs, defStyleAttr) {
     private lateinit var mFragmentAdapter: BaseFragmentPagerAdapter
-    private lateinit var mCouponFragment: MyPageCouponFragment
-    private lateinit var mDisabledCouponFragment: MyPageCouponFragment
 
     override fun getBaseTag() = this::class.simpleName.toString()
     override fun getLayoutId() = R.layout.customlayout_mypage_coupon
     override fun init() {
-        initViewModel()
-        initTabs()
-        initViewPager()
-
+        mViewModel = MyPageCouponViewModel()
+        mBinding.tablayoutMypagecoupon.setupWithViewPager(mBinding.viewpagerMypagecoupon)
         mBinding.viewModel = mViewModel
+
+        initViewPager()
         mBinding.executePendingBindings()
-    }
-
-    private fun initViewModel() {
-        mViewModel = MyPageCouponViewModel(context)
-        mViewModel.enabledCouponResponse.observe(this, Observer {
-            setEnabledCouponTabText(totalElements = mViewModel.enabledCouponResponse.value?.totalElements
-                    ?: 0)
-            updateCouponFragment(isAvailable = true, list = it.content)
-        })
-        mViewModel.disabledCouponResponse.observe(this, Observer { updateCouponFragment(isAvailable = false, list = it.content) })
-    }
-
-    private fun setEnabledCouponTabText(totalElements: Int) {
-        val text = resources.getString(R.string.mypagecoupon_tab_enabled, totalElements)
-        if (mBinding.tablayoutMypagecoupon.getTabAt(0)?.text != text) mBinding.tablayoutMypagecoupon.getTabAt(0)?.text = text
-    }
-
-    private fun updateCouponFragment(isAvailable: Boolean, list: MutableList<Coupon>) {
-        val fragment = if (isAvailable) mCouponFragment else mDisabledCouponFragment
-        if (mViewModel.page > 1) fragment.addCoupons(list)
-        else fragment.setCoupons(list)
-
-        fragment.checkEmptyVisible()
-//        fragment.checkMoreVisible()
     }
 
     private fun initViewPager() {
         mFragmentAdapter = BaseFragmentPagerAdapter((context as FragmentActivity).supportFragmentManager).apply {
             this.mTabTitles = mutableListOf(resources.getString(R.string.mypagecoupon_tab_enabled_temp), resources.getString(R.string.mypagecoupon_tab_disabled))
         }
-        mCouponFragment = MyPageCouponFragment().apply {
-            mIsAvailable = true
-            mViewModel = this@MyPageCouponLayout.mViewModel
-        }
-        mDisabledCouponFragment = MyPageCouponFragment().apply {
-            mIsAvailable = false
-            mViewModel = this@MyPageCouponLayout.mViewModel
-        }
-        mFragmentAdapter.addFragment(mCouponFragment)
-        mFragmentAdapter.addFragment(mDisabledCouponFragment)
+        mFragmentAdapter.addFragment(createCouponFragment(isAvailable = true))
+        mFragmentAdapter.addFragment(createCouponFragment(isAvailable = false))
         mBinding.viewpagerMypagecoupon.adapter = mFragmentAdapter
     }
 
-    private fun initTabs() {
-        mBinding.tablayoutMypagecoupon.setupWithViewPager(mBinding.viewpagerMypagecoupon)
-    }
+    private fun createCouponFragment(isAvailable: Boolean): MyPageCouponFragment =
+            MyPageCouponFragment().apply {
+                mIsAvailable = isAvailable
+                mViewModel = MyPageCouponViewModel()
+                if (isAvailable) mSetTabTitleTask = { tabText -> mBinding.tablayoutMypagecoupon.getTabAt(0)?.text = tabText }
+            }
 
     override fun onFocusView() {}
 
